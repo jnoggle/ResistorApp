e80e936b3d906a2469b0e5fc8e479c3f75e72979
Justin Noggle <justin.noggle@gmail.com>
2017-02-08 14:22:47 -0600

    Initial commit: Create MainActivity and framework for OpenGL Rendering

---
+++ app/src/main/java/edu/ksu/poly/acm/resistorapp/MainActivity.java
@@ -0,0 +1,22 @@
+package edu.ksu.poly.acm.resistorapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void show(View view) {
+        Intent intent = new Intent(this, OpenGLES20Activity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/edu/ksu/poly/acm/resistorapp/OpenGLES20Activity.java
@@ -0,0 +1,38 @@
+package edu.ksu.poly.acm.resistorapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OpenGLES20Activity extends Activity {
+
+    private GLSurfaceView mGLView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Create a GLSurfaceView instance and set it
+        // as the Content View for this Activity
+        mGLView = new MyGLSurfaceView(this);
+        setContentView(mGLView);
+    }
+
+    class MyGLSurfaceView extends GLSurfaceView {
+        private final MyGLRenderer mRenderer;
+
+        public MyGLSurfaceView(Context context) {
+            super(context);
+
+            // Create an OpenGL ES 2.0 context
+            setEGLContextClientVersion(2);
+
+            mRenderer = new MyGLRenderer();
+
+            // Set the Renderer for drawing on the GLSurfaceView
+            setRenderer(mRenderer);
+        }
+    }
+}
